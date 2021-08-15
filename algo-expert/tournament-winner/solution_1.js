71e0e4df8999c27564bb75708cfc5757eb99b8f8
MohammadArsalan <a4arshi@yahoo.com>
2021-08-15 12:57:01 +0500

    tournament-winner solution_1

---
+++ algo-expert/tournament-winner/solution_1.js
@@ -0,0 +1,45 @@
+/**
+    "competitions": [
+        ["HTML", "C#"],
+        ["C#", "Python"],
+        ["Python", "HTML"]
+    ],
+  "results": [0, 0, 1]
+
+  there are two teams 1 - home team 0 - away team;
+
+  time complexity: O(n) | O(k) space complexity where the k is number of teams.
+ */
+
+const HOME_TEAM_WON = 1;
+const tournamentWinner = function (compititions, results) {
+    let bestTeam = '';
+    const scores = { [bestTeam]: 0 };
+
+    for (let i = 0; i < compititions.length; i++) {
+        const [homeTeam, awayTeam] = compititions[i];
+
+        const winner = results[i] === HOME_TEAM_WON ? homeTeam : awayTeam;
+
+        if (scores[winner]) {
+            scores[winner] += 3
+        } else {
+            scores[winner] = 3
+        }
+
+        if(scores[winner] > scores[bestTeam]) {
+            bestTeam = winner;
+        }
+    }
+
+    return bestTeam;
+}
+
+const compititions = [
+    ["HTML", "Javascript"],
+    ["Javascript", "Python"],
+    ["Python", "HTML"]
+]
+
+const results = [0 , 1, 1];
+console.log(tournamentWinner(compititions, results));
