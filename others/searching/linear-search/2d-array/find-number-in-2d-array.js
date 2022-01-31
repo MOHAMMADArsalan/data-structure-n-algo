d0885eef2c6d6f4692c42a26631949cd7ed4d624
MohammadArsalan <a4arshi@yahoo.com>
2022-01-31 16:11:06 +0500

    find number in 2 d array linear search

---
+++ others/searching/linear-search/2d-array/find-number-in-2d-array.js
@@ -0,0 +1,25 @@
+
+function findNumberIn2DArray(array, target) {
+    for(let row = 0; row < array.length;row++) {
+        for(let col = 0; col < array[row].length; col++) {
+            if(array[row][col] === target) {
+                return [row, col];
+            }
+        }
+    }
+    return [-1, -1]
+}
+
+
+
+const array = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
+console.log(findNumberIn2DArray(array, 7));
+console.log(findNumberIn2DArray(array, 17));
+console.log(findNumberIn2DArray(array, 16));
+// 7
+
