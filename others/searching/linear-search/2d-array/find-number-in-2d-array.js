03f478335434b14d8f3911c2710012615dfc0ac1
MohammadArsalan <a4arshi@yahoo.com>
2022-02-01 09:58:41 +0500

    add find  Number in 2 D array

--- others/searching/linear-search/2d-array/find-number-in-2d-array.js
+++ others/searching/linear-search/2d-array/find-number-in-2d-array.js
@@ -11,6 +11,22 @@ function findNumberIn2DArray(array, target) {
 }
 
 
+function findNumberIn2DArray2(array, target) {
+    let row = 0, col = array[0].length - 1;
+
+    while (row < array.length && col >= 0) {
+        if (array[row][col] === target) return [row, col];
+        else if (array[row][col] < target) {
+            row++;
+        } else {
+            col--;
+        }
+    }
+
+    return [-1, -1];
+
+
+}
 
 const array = [
     [1, 2, 3, 4],
